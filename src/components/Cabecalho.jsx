588a34e59faafd58290c9feb90f15ee99b83f9b2
Enzo Belizardo <enzo.s.belizardo@outlook.com>
2026-09-23 09:18:05 -0300

    Refactor Cabecalho component with new structure and links

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,12 +1,17 @@
 function Cabecalho() {
   return (
-    <header className="cabecalho">
-      <div className="cabecalho-conteudo">
-        <h1>Sistema de Gestão de Comércio</h1>
-        <p>Gerenciamento de produtos, clientes e funcionários</p>
+    <header className="header-container">
+      <div className="header-info">
+        <p className="docente-info">Profa Mestre Sirley Ambrosia Vitorio Addão</p>
+        <h1 className="aluno-info">Desenvolvido por: Enzo Belizardo</h1>
       </div>
+      <nav className="header-nav">
+        <Link to="/">Início</Link>
+        <Link to="/chamados">Gerenciamento</Link>
+        <Link to="/chamados/listar">Listar Chamados</Link>
+        <Link to="/chamados/cadastrar">Novo Chamado</Link>
+      </nav>
     </header>
-  )
+  );
 }
-
 export default Cabecalho
